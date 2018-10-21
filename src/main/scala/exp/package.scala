1080dd72e99265e44bdf901ab8d360e96d547700
Karel Smutny <karel.smutny@outlook.com>
2018-10-21 22:04:12 +0200

    Added printing of a Sum

--- src/main/scala/exp/package.scala
+++ src/main/scala/exp/package.scala
@@ -3,5 +3,6 @@ package object exp {
   def print(expression: Expression): String = expression match {
     case Number(n) => n.toString
     case Variable(name) => name
+    case Sum(terms) => terms map print mkString " + "
   }
 }
--- src/test/scala/exp/PrintSpec.scala
+++ src/test/scala/exp/PrintSpec.scala
@@ -2,7 +2,14 @@ package exp
 
 import org.scalatest.FunSuite
 
+import scala.language.implicitConversions
+
 class PrintSpec extends FunSuite {
+
+  implicit def intToNumber(n: Int): Number = Number(n)
+  implicit def stringToVariable(name: String): Variable= Variable(name)
+
+  def sum(terms: Expression*) = Sum(terms.toList)
 
   test("single number") {
     val exp = Number(10)
@@ -12,5 +19,20 @@ class PrintSpec extends FunSuite {
   test("single variable") {
     val exp = Variable("x")
     assert(print(exp) == "x")
+  }
+
+  test("sum of numbers") {
+    val exp = sum(1, 2, 3)
+    assert(print(exp) == "1 + 2 + 3")
+  }
+
+  test("sum of numbers and variables") {
+    val exp = sum(1, "a", 3, "b")
+    assert(print(exp) == "1 + a + 3 + b")
+  }
+
+  test("sum of sums") {
+    val exp = sum(sum(1, "a"), sum(3, "b"))
+    assert(print(exp) == "1 + a + 3 + b")
   }
 }
